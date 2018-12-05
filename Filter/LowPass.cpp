754aff570ee9f34f4c6379f9335322a4452cfab8
maxbachmann <kontakt@maxbachmann.de>
2018-12-05 23:39:13 +0100

    moved operators to passFilter bas class

--- Filter/LowPass.cpp
+++
@@ -1,9 +0,0 @@
-#include "LowPass.hpp"
-#include "Bandpass.hpp"
-
-std::unique_ptr<Bandpass> operator+ (
-    std::shared_ptr<LowPass> const lowPass,
-    std::shared_ptr<HighPass> const highPass)
-{
-    return std::make_unique<Bandpass>(lowPass, highPass);
-}
---
+++ Filter/PassFilter.cpp
@@ -0,0 +1,19 @@
+#include "PassFilter.hpp"
+
+#include "HighPass.hpp"
+#include "LowPass.hpp"
+#include "Bandpass.hpp"
+
+std::unique_ptr<Bandpass> operator+ ( 
+    std::shared_ptr<HighPass> const highPass,
+    std::shared_ptr<LowPass> const lowPass)
+{
+    return std::make_unique<Bandpass>(lowPass, highPass);
+}
+
+std::unique_ptr<Bandpass> operator+ (
+    std::shared_ptr<LowPass> const lowPass,
+    std::shared_ptr<HighPass> const highPass)
+{
+    return std::make_unique<Bandpass>(lowPass, highPass);
+}
