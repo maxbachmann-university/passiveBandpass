6d7be564303a0aa207e3833dace2b43dc62571ed
maxbachmann <kontakt@maxbachmann.de>
2018-12-06 20:09:02 +0100

    updated + operator

--- Filter/PassFilter.cpp
+++ Filter/PassFilter.cpp
@@ -20,7 +20,29 @@ std::unique_ptr<Filter> operator+ (
     std::shared_ptr<PassFilter> const Filter1,
     std::shared_ptr<PassFilter> const Filter2)
 {
+    const FilterType type1 = Filter1->m_type;
+    const FilterType type2 = Filter2->m_type;
+    std::unique_ptr<Filter> retFilter;
 
-    return std::make_unique<Bandpass>(Filter1, Filter2);
+    if (type1 != type2)
+    {
+        if (type1 == FilterType::LowPass)
+        {
+            retFilter = std::make_unique<Bandpass>(Filter1, Filter2);
+        }
+        else{
+            retFilter = std::make_unique<Bandpass>(Filter2, Filter1);
+        }
+    }
+    else{
+        if (type1 == FilterType::LowPass)
+        {
+            //Pointer to combined Filter two LowPass
+        }
+        else{
+            //Pointer to combined Filter two HighPass
+        }
+    }
+    return retFilter;
 }
 
